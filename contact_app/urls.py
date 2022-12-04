ffa50bb9afc10144ac482f3058c6ffdd80de2c84
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-12-04 15:25:59 +0530

    added new apps

---
+++ contact_app/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from contact_app import views
+urlpatterns = [
+    path('', views.dashboard, name='dashboard')
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+@login_required(login_url='auth/login')
+def home(request):
+    # return HttpResponse("Hello to Home Page")
+    context = {}
+    return render(request, 'main/home.html', context)
